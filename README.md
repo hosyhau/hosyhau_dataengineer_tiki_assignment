c9ee051d3f2f2edb3c40158c15ade0e1224d4bae
hosyhau <hosyhau97haui@gmail.com>
2019-03-31 14:49:39 +0700

    hauhs: - add comment in the code.        - write a document for testing.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # hosyhau_dataengineer_tiki_assignment
-
-This is hosyhau's assignment for the tiki's test.
+1. System required:
+	- Java 8 or over
+	- maven 3
+	- can be connected to the internet and maven repository
+2. Guideline to set up application:
+	- run application:
+		+) mvn clean package -Dskip test
+		+) java -jar ./target/tiki-application.jar
+	- run unit test coverage:
+		+) mvn test	
--- tiki-application/src/main/java/tiki/vn/assignment/TIkiApplication.java
+++ tiki-application/src/main/java/tiki/vn/assignment/TIkiApplication.java
@@ -15,20 +15,27 @@ public class TIkiApplication {
 
     public static void main(String[] args) {
 
+        // we can config the parameter to run specific service which we want to execute.
+        // if param is ONE, it means you want to create one product
+        // if param is COMBINATION, it means you want to a product that can be combined a range of products which is the same name.
         ProductService productOneService = FactoryProvider.getProductService(ProductType.ONE);
         ProductService productCombineService = FactoryProvider.getProductService(ProductType.COMBINATION);
         ProductRepository productRepository = new ProductRepository();
 
+        // initial data for 3 tests
         ProductDto productDtoTest1 = productRepository.createProductDtoForTest1();
         ProductDto productDtoTest2 = productRepository.createProductDtoForTest2();
         List<ProductDto> productDtosTest3 = productRepository.createProductDtoForTest3();
 
+        // run test 1
         Product productTest1 = (Product) productOneService.createProduct(productDtoTest1);
         System.out.println("Test 1 = "+productTest1);
 
+        // run test 2
         Product productTest2 = (Product) productOneService.createProduct(productDtoTest2);
         System.out.println("Test 2 = "+productTest2);
 
+        // run test 3
         Map<String, Set<Product>> productTest3 = (Map<String, Set<Product>>) productCombineService.createProduct(productDtosTest3);
         System.out.println("Test 3 = "+productTest3);
     }
